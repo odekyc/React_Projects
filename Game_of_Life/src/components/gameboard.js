599442f6d9d29be25b2221af40f6b8e6562e357f
Elva Chao <odekyc@Elvas-Air.championbroadband.com>
2016-06-02 07:43:15 -0700

    game of life added

--- Game_of_Life/src/components/gameboard.js
+++ Game_of_Life/src/components/gameboard.js
@@ -11,6 +11,18 @@ export default class Gameboard extends Component {
     <div>
   	
   	<div id="upperpad">
+    <div id="upperbut">
+    <button id="top1">
+    Run
+    </button>
+      <button id="top2">
+    Pause
+    </button>
+        <button id="top3">
+    Clear
+    </button>
+    </div>
+    <p id="gen_count">Generation:</p>
     </div>
     <div id="gameboard" >
       
