68fd4f77d00851ec0a4de1790650051d0bc2c2ec
Elva Chao <odekyc@Elvas-Air.lan>
2016-08-10 14:22:24 -0700

    hi

--- Game_of_Life/src/components/app.js
+++ Game_of_Life/src/components/app.js
@@ -1,6 +1,99 @@
 import React from 'react';
 import { Component } from 'react';
-import Gameboard from './gameboard';
+
+//dumb components, not involved in dispatching actions and has nothing to do with 
+//updating states in redux
+const Button=({id, title, handleClick})=>(
+
+    <button id={id} onClick={handleClick}>{title}</button>      
+
+);
+
+const Counter=({genCount})=>(
+     
+     <p id="gen_count">Generation :  {genCount} </p>
+);
+
+
+
+class Grid extends Component {
+
+	render(){
+
+		return (
+        <div id="grid">
+         <div id='span'>
+         </div>
+         
+         </div>
+	);
+	}
+
+    
+}
+
+class Gameboard_ extends Component {
+
+	render(){
+
+		return (
+    
+         <div id="gameboard" >
+
+           <Grid />
+          </div>
+	);
+	}
+
+
+}
+
+class Upperpad_ extends Component{
+
+	render(){
+
+      return(
+
+       <div id="upperpad">
+    <div id="upperbut">
+     <Button id={"top1"} title={"Run"}></Button>
+      <Button id={"top2"} title={"Pause"}></Button>
+        <Button id={"top3"} title={"Clear"}></Button>
+    </div>
+     <Counter genCount={900000}></Counter>
+    </div>
+
+     );
+
+	};
+}
+
+class Lowerpad_ extends Component{
+
+	render(){
+
+      return(
+         <div id="lowerpad">
+    <div>
+    <p id="board_sz">Board Size:</p>
+    <br />
+    <p id="sim_spd"> Sim Speed</p>
+    <div id="lowerbuts">
+    <Button id={"bottom1"} title={"Size: 50X30"}></Button>
+    <Button id={"bottom2"} title={"Size:70X50"}></Button>
+    <Button id={"bottom3"} title={"Size:100X80"}></Button>
+    <Button id={"bottom4"} title={"SLOW"}></Button>
+    <Button id={"bottom5"} title={"MEDIUM"}></Button>
+    <Button id={"bottom6"} title={"FAST"}></Button>
+    </div>
+    </div>
+    </div>
+      
+
+     );
+
+	};
+}
 
 
 export default class App extends Component {
@@ -8,12 +101,17 @@ export default class App extends Component {
 	render(){
 
 		return (
-    <div>
+    
+  	     <div>
   	
-  	<Gameboard />
-  
-  
+  	  <Upperpad_/>
+     <Gameboard_/>
+     <Lowerpad_/>
     </div>
+  	
+  
+  
+    
 	);
 	}
 
